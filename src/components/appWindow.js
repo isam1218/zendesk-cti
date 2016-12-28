29e004a91b5864cba76cdeba92dbc96f26f04de7
Sean Rose <sean1rose@gmail.com>
2016-12-28 15:02:17 -0800

    ZI-38: STILL IN PROGRESS: default view - get something on the screen to work with @clnielsen

--- src/components/appWindow.js
+++ src/components/appWindow.js
@@ -12,6 +12,13 @@ export default class AppWindow extends Component {
   
   constructor(props) {
     super(props);
+    // this.state.phone is the phone # dialed into form input
+    this.state = {
+      screen: 'basic',
+      phone: '',
+      focused: false,
+      popup: null
+    }
     // initial feed data from index.js
   }
 
@@ -27,41 +34,111 @@ export default class AppWindow extends Component {
     console.log('appWindow.js after props set as state - ', this.state);
   }
 
-  // THIS IS JUST AN EXAMPLE OF WHAT COULD BE RENDERED REFER TO HUDN LOGIC 
+  // change view
+  _changeScreen(type = '') {
+    this.setState({
+      screen: type,
+      phone: ''
+    });
+    console.log('_changeScreen to -> ', type);
+  }
+
+  // handles css depending on focus change of Number/Extension input
+  _setFocus(f) {
+    console.log('called _setFocus - ', f);
+    this.setState({
+      focused: f
+    });
+  }
+
+  // handles input event.target.value
+  _updateValue(e, property) {
+    console.log('in _updateValue - ', e, property);
+    this.setState({
+      [property]: e.target.value
+    })
+  }
+
+  // press enter or green call button to call
+  _callNumber(e) {
+    console.log('in _callNumber - ',e);
+    // can press enter to call
+    if (e && e.key != 'Enter')
+      return;
+    
+    if (this.state.phone != '') {
+      // this._sendAction('call', this.state.phone);
+
+      // NEED TO CHANGE TO DIALPAD SCREEN
+      // clear form + change to dialpad screen? or to call screen?
+      // this.setState({
+      //   phone: '',
+      //   screen: 'call'
+      // });
+
+      this._changeScreen('call');
+
+      e.target.blur();
+    }
+  }
+
+  // parse out unwanted inputs
+  _restrictInput(e) {
+    console.log('_restrictInput called - ', e);
+    // convert letters and strip out unwanted chars
+		e.target.value = e.target.value
+			.replace(/[abc]/ig, 2)
+			.replace(/[def]/ig, 3)
+			.replace(/[ghi]/ig, 4)
+			.replace(/[jkl]/ig, 5)
+			.replace(/[mno]/ig, 6)
+			.replace(/[pqrs]/ig, 7)
+			.replace(/[tuv]/ig, 8)
+			.replace(/[wxyz]/ig, 9)
+			.replace(/[^0-9\*\+\-()# ]/g, '');
+  }
+
+  // calls relevant popup
+  _openPopup(type) {
+    console.log('in _openPopup');
+    this.setState({
+      popup: type
+    });
+  }
   
+  // AT THIS POINT, WE'RE JUST TESTING TO GET SOME STUFF ON THE SCREEN
   render() {
-    // pass feed data down the chain as props
     // if (this.state && this.state.locations &&  this.state.locations[this.state.settings.current_location] && this.state.locations[this.state.settings.current_location].name){
-    // if location name is available -> display it
-    if (this.state && this.state.locations && Object.keys(this.state.locations).length > 1){
+    // if not on call, this is [DEFAULT SCREEN - BASIC WINDOW NO CALL]
+    if (this.state && this.state.screen == 'basic' && this.state.mycalls && this.state.mycalls.length == 0 && this.state.locations &&  this.state.locations[this.state.settings.current_location] && this.state.locations[this.state.settings.current_location].name){
       // console.log('appWindow.js: 3a rendering app w/ data  this.state is - ', this.state);
-      // AT THIS POINT, WE'RE JUST TESTING TO GET SOME STUFF ON THE SCREEN
-      var audioBtn, dialBtn, body;
-      
-      // var formCSS = 'form' + (this.state.focused ? ' focused' : '');
+      var audioBtn, body;
+      var formCSS = 'form' + (this.state.focused ? ' focused' : '');
+      // var formCSS = 'form focused';
       // var callBtnCSS = 'material-icons callbtn' + (this.state.phone != '' ? ' active' : '');
+      var callBtnCSS = 'material-icons callbtn';
       // var audioBtnCSS = 'material-icons audio';
-      
+      var dialBtn = (<i className="material-icons dialpad" onClick={() => this._changeScreen('dialpad')}>dialpad</i>);
       // extra buttons for softphone users
-      if (this.props.locations[this.props.settings['current_location']].locationType == 'w') {
-        var classy = 'material-icons audio';
+      // if (this.props.locations[this.props.settings['current_location']].locationType == 'w') {
+      //   console.log('in extra buttons if branch!');
+      //   var classy = 'material-icons audio';
       
-        // if (this.state.popup == 'audio')
-        //   classy += ' on';
+      //   // if (this.state.popup == 'audio')
+      //   //   classy += ' on';
         
-        audioBtn = (<i className={classy} onClick={() => this._openPopup('audio')}>volume_up</i>);
+      //   // audioBtn = (<i className={classy} onClick={() => this._openPopup('audio')}>volume_up</i>);
         
-        dialBtn = (<i className="material-icons dialpad" onClick={() => this._changeScreen('dialpad')}>dialpad</i>);
-      }
+      //   // dialBtn = (<i className="material-icons dialpad" onClick={() => this._changeScreen('dialpad')}>dialpad</i>);
+      // }
     
       body = (
         <div id="basic">  
-          {audioBtn}
         
           <div className="location">
             <span>Location:</span>
-            <span className="my_location" onClick={this.props.settings.chat_status != 'offline' ? () => this._openPopup('location') : ''}>
-              {this.props.locations[this.props.settings.current_location].name} 
+            <span className="my_location" onClick={this.state.settings.chat_status != 'offline' ? () => this._openPopup('location') : ''}>
+              {this.state.locations[this.state.settings.current_location].name} 
               <i className="material-icons">expand_more</i>
             </span>
           </div>
@@ -94,32 +171,21 @@ export default class AppWindow extends Component {
 
         
           <div id="header">
-            <div onClick={() => this._openPopup('status')}>
+            <div>
               <div ></div>
-              <i className="material-icons more">expand_more</i>
+              FONALITY
             </div>
           
             <div className="buttons">
-              <div className="missed_calls">
-                <div className="tooltip">Open HUD for details.</div>
-                <i className="material-icons">call_missed</i>
-                
-              </div>
               
-              <div className="voicemails">
-                <div className="tooltip">Open HUD for details.</div>
-                <i className="material-icons">voicemail</i>
-                
-              </div>
-              
-              <div className="chats">
-                <div className="tooltip">Open HUD for details.</div>
-                <i className="material-icons">chat</i>
+              <div className="agentlogin">
+                <div className="tooltip"></div>
+                <i className="material-icons">headset</i>
                 
               </div>
             </div>
             
-            <i className="material-icons" onClick={() => this._openPopup('preferences')}>settings</i>
+            <i className="material-icons" onClick={() => this._openPopup('preferences')}>power_settings_new</i>
           </div>
           
           {body}
@@ -162,6 +228,12 @@ export default class AppWindow extends Component {
       */}     
 
 
+    }
+    else if (this.state.screen == 'dialpad') {
+      console.log("DIAL PAD SCREEN GOES HERE!");
+    }
+    else if (this.state.screen == 'call') {
+      console.log('CALL SCREEN GOES HERE!');
     }
     else {
       // console.log('appWindow.js: 3b not rendering view w/ data yet...');
