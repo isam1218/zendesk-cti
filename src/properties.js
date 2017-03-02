d1eabe76c4ca2650c6365a873c7ef96a3dfb1cfc
Isam Shabandri <ishabandri@fonality.com>
2017-03-02 11:21:43 -0800

    Changed properties.js to stage4 url

--- src/properties.js
+++ src/properties.js
@@ -1,11 +1,11 @@
 
 
 export default {
-    serverURL:"https://dev4.fon9.com:8081"
+    //serverURL:"https://dev4.fon9.com:8081"
     //serverURL:"https://huc-qa.fonality.com:8080"
     //serverURL:"https://huc-dev.fonality.com:8081"
     //serverURL:"https://lb-stage3.fonality.com:446"
-    //serverURL:"https://lb-stage4.fonality.com:446"
+    serverURL:"https://lb-stage4.fonality.com:446"
   
 }
 
