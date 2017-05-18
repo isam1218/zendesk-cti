1afa41582661e085e0e010a031c77a8e791f1e65
Isam Shabandri <ishabandri@fonality.com>
2017-05-18 10:38:37 -0700

    Fixed IE bug

--- src/components/appWindow.js
+++ src/components/appWindow.js
@@ -45,19 +45,49 @@ export default class AppWindow extends Component {
 
 
     componentDidMount() {
+            // Opera 8.0+
+var isOpera = (!!window.opr && !!opr.addons) || !!window.opera || navigator.userAgent.indexOf(' OPR/') >= 0;
 
+// Firefox 1.0+
+var isFirefox = typeof InstallTrigger !== 'undefined';
+
+// Safari 3.0+ "[object HTMLElementConstructor]" 
+var isSafari = /constructor/i.test(window.HTMLElement) || (function (p) { return p.toString() === "[object SafariRemoteNotification]"; })(!window['safari'] || safari.pushNotification);
+
+// Internet Explorer 6-11
+var isIE = /*@cc_on!@*/false || !!document.documentMode;
+
+// Edge 20+
+var isEdge = !isIE && !!window.StyleMedia;
+
+// Chrome 1+
+var isChrome = !!window.chrome && !!window.chrome.webstore;
+
+// Blink engine detection
+var isBlink = (isChrome || isOpera) && !!window.CSS;
+
+                   
 
         //if (this.state.screen == "default" || this.state.screen == "settings" ) {
             window.addEventListener('storage', (e) => {
                 
                 if(e.key == "errorTicket"){
-                   
-                    if (e.newValue != null && !fdp.master) {
+
+                    if(isIE){
+                    if ((e.newValue != (null || "")) && (e.oldValue == (null || ""))) {
                         client.invoke('notify', e.newValue, "error", 10000);
                         
                     }
-                    if(fdp.master && e.oldValue != null){
-                        client.invoke('notify', e.oldValue, "error", 10000);
+                    }
+                   
+                   if(isChrome){
+                        if (e.newValue != null && !fdp.master) {
+                            client.invoke('notify', e.newValue, "error", 10000);
+                            
+                        }
+                        if(fdp.master && e.oldValue != null){
+                            client.invoke('notify', e.oldValue, "error", 10000);
+                        }
                     }
 
                     localStorage.removeItem("errorTicket");
